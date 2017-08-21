bc67d8aff80e318f7a8e88eba70b6721979e9604
Soumya Gupta <soumyagupta.ubc@gmail.com>
2017-08-20 23:21:37 -0700

    included search plugin and recent addition of lightbox

---
+++ js/main.js
@@ -0,0 +1,36 @@
+$('img').on('click', function(){
+  $(this).toggleClass('selected');
+});
+
+(function() {
+  var $imgs = $('#gallery li img');
+  var $search = $('#search');
+  var cache =[];
+  $imgs.each(function(){
+      cache.push({
+        element: this,
+        text: this.alt.trim().toLowerCase()
+      });
+  });
+function filter(){
+  var query = this.value.trim().toLowerCase();
+
+  cache.forEach(function (img){
+  var index = 0;
+  if (query) {
+    index = img.text.indexOf(query);
+  }
+  img.element.style.display = index === -1 ? 'none' : '';
+});
+}
+if ('oninput in $search[0]'){
+  $search.on('input', filter);
+} else {
+  $search.on('keyup', filter);
+}
+}());
+
+
+$(document).ready(function() {
+  $('.image-link').magnificPopup({type:'image'});
+});
